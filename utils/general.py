1e8896c3135618831ed06cdc1bd2a80cc22a7cca
santius0 <santius1995@gmail.com>
2022-04-10 20:09:32 -0400

    added metric computations

--- utils/general.py
+++ utils/general.py
@@ -1,10 +1,12 @@
 import os
 import glob
 import pandas as pd
+import numpy as np
 from datetime import date, timedelta
 import string
 from functools import wraps
 from time import time
+import math
 
 
 def all_day_in_year(day=0, year=date.today().year):
@@ -29,7 +31,8 @@ def files_in_path(path):
     return glob.glob(path)
 
 
-def merge_csvs_in_path(path, glob_pattern="hot-100_*.csv", output_path='../data/billboard', output_filename='merged_csv', index=False):
+def merge_csvs_in_path(path, glob_pattern="hot-100_*.csv", output_path='../data/billboard',
+                       output_filename='merged_csv', index=False):
     files = glob.glob(f'{os.path.abspath(path)}/{glob_pattern}')
     full_df = None
     for file in files:
@@ -64,6 +67,26 @@ def execution_time(func):
         st = time()
         ret = func(*args, **kwargs)
         et = time()
-        print('\n func:%r args:[%r, %r] took: %2.2f sec' % (func.__name__, args, kwargs, et-st))
+        print('\n func:%r args:[%r, %r] took: %2.2f sec' % (func.__name__, args, kwargs, et - st))
         return ret
+
     return wrapper
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+def tanh(x):
+    return (2 * sigmoid(2 * x)) - 1
+
+
+def squiggle(rank_counts: np.ndarray or list, ranks: np.ndarray or list, peak: int or None = None) -> float:
+    s = 0
+    highest_rank = -math.inf
+    for i in range(len(rank_counts)):
+        s += rank_counts[i] * 1 / (ranks[i])
+        if ranks[i] > highest_rank:
+            highest_rank = ranks[i]
+    peak = highest_rank if peak is None else peak
+    return tanh((1 / peak) * s)
